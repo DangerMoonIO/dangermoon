b7dae5b105556c8afcaa6dfb96f62a88ac7cebc4
Jason Hall <jason13@vt.edu>
2021-07-14 15:30:42 -0400

    Changes for DangerMoonV2. /up

--- scripts/deployBscMainNet.js
+++ scripts/deployBscMainNet.js
@@ -20,19 +20,20 @@ async function main() {
   const VRF_COORDINATOR = "0x747973a5A2a4Ae1D3a8fDF5479f1514F65Db9C31";
   const LINK_ADDRESS = '0x404460C6A5EdE2D891e8297795264fDe62ADBB75';
   const LINK_KEYHASH = "0xc251acd21ec4fb7f31bb8868288bfdbaeb4fbfec2df3735ddbd4f7dc8d60103c";
-  const dangermoon = await DangerMoon.deploy(UNISWAP_ROUTER, VRF_COORDINATOR, LINK_ADDRESS, LINK_KEYHASH);
+  const CHARITY_ADDRESS = "0x5c0e5E0Ccb0DED355EcE0cB6f56B2CE75CB0b8eD";
+  const MARKETING_ADDRESS = "0x7817279e3510C2385536b0901de14fCC29e36878";
+  const dangermoon = await DangerMoon.deploy(
+    UNISWAP_ROUTER,
+    VRF_COORDINATOR,
+    LINK_ADDRESS,
+    LINK_KEYHASH,
+    CHARITY_ADDRESS,
+    MARKETING_ADDRESS
+  );
 
   await dangermoon.deployed();
 
   console.log("DangerMoon deployed to bsc mainnet:", dangermoon.address);
-
-  // halfTotalTokenSupply = (await dangermoon.totalSupply()).div(2).toString();
-  // // await safemoon.approve(UNISWAP_ROUTER, halfTotalTokenSupply);
-  //
-  // const DEAD = '0x000000000000000000000000000000000000dEaD';
-  // await dangermoon.transfer(DEAD, halfTotalTokenSupply);
-  // console.log("half total token supply burnt");
-
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- test/Dangermoon.js
+++ test/Dangermoon.js
@@ -173,32 +173,36 @@ const DEAD = '0x000000000000000000000000000000000000dEaD';
 let dangermoon;
 const dangermoonDecimals = 9;
 let owner;
-let alice;
-let bob;
+let a;
+let b;
+let c;
+let d;
+let marketing;
+let charity;
 let testWallet;
 let halfTotalTokenSupply
 
 async function logAllBalances(header) {
   console.log(header);
-  console.log("lifetimeReflection", (await dangermoon.lifetimeReflection()).toString());
+  console.log("totalReflected", (await dangermoon.totalReflected()).toString());
   console.log("currentReflection", (await dangermoon.currentReflection()).toString());
   console.log("0xdead", (await dangermoon.balanceOf(DEAD)).toString());
   console.log("0xowner", (await dangermoon.balanceOf(owner.address)).toString());
-  console.log("0xalice", (await dangermoon.balanceOf(alice.address)).toString());
-  console.log("0xbob", (await dangermoon.balanceOf(bob.address)).toString());
-  console.log("0xcindy", (await dangermoon.balanceOf(cindy.address)).toString());
+  console.log("0xa", (await dangermoon.balanceOf(a.address)).toString());
+  console.log("0xb", (await dangermoon.balanceOf(b.address)).toString());
+  console.log("0xc", (await dangermoon.balanceOf(c.address)).toString());
   console.log("\n");
 }
 
 async function expectAllBalances(expectations) {
-  const { _lifetimeReflection, _currentReflection, _dead, _owner, _alice, _bob, _cindy } = expectations;
-  expect( (await dangermoon.lifetimeReflection()).toString(), "lifetimeReflection").to.equal(_lifetimeReflection);
-  expect( (await dangermoon.currentReflection()).toString(),   "currentReflection").to.equal(_currentReflection);
-  expect( (await dangermoon.balanceOf(DEAD)).toString(),                  "0xdead").to.equal(_dead);
-  expect( (await dangermoon.balanceOf(owner.address)).toString(),          "owner").to.equal(_owner);
-  expect( (await dangermoon.balanceOf(alice.address)).toString(),          "alice").to.equal(_alice);
-  expect( (await dangermoon.balanceOf(bob.address)).toString(),              "bob").to.equal(_bob);
-  expect( (await dangermoon.balanceOf(cindy.address)).toString(),          "cindy").to.equal(_cindy);
+  const { _totalReflected, _currentReflection, _dead, _owner, _a, _b, _c } = expectations;
+  expect( (await dangermoon.totalReflected()).toString(),       "totalReflected").to.equal(_totalReflected);
+  expect( (await dangermoon.currentReflection()).toString(), "currentReflection").to.equal(_currentReflection);
+  expect( (await dangermoon.balanceOf(DEAD)).toString(),                "0xdead").to.equal(_dead);
+  expect( (await dangermoon.balanceOf(owner.address)).toString(),        "owner").to.equal(_owner);
+  expect( (await dangermoon.balanceOf(a.address)).toString(),                "a").to.equal(_a);
+  expect( (await dangermoon.balanceOf(b.address)).toString(),                "b").to.equal(_b);
+  expect( (await dangermoon.balanceOf(c.address)).toString(),                "c").to.equal(_c);
 }
 
 async function buyFromUniswap(buyer, faucetEther, etherToSpend) {
@@ -207,7 +211,7 @@ async function buyFromUniswap(buyer, faucetEther, etherToSpend) {
     value: ethers.utils.parseEther(faucetEther)
   });
   await uniswapContract.swapETHForExactTokens(
-    "1000000000000000000000",
+    "111111111111111111",
     [WETH_ADDRESS, dangermoon.address],
     buyer.address,
     Math.floor(Date.now() / 1000),
@@ -220,11 +224,18 @@ async function buyFromUniswap(buyer, faucetEther, etherToSpend) {
 
 describe("DangerMoon", function () {
   beforeEach(async () => {
-    [owner, alice, bob, cindy] = await ethers.getSigners();
+    [owner, a, b, c, d, marketing, charity] = await ethers.getSigners();
 
     // Get and deploy dangermoon
     const MockDangerMoon = await ethers.getContractFactory("MockDangerMoon");
-    dangermoon = await MockDangerMoon.deploy(UNISWAP_ROUTER, VRF_COORDINATOR, LINK_ADDRESS, LINK_KEYHASH);
+    dangermoon = await MockDangerMoon.deploy(
+      UNISWAP_ROUTER,
+      VRF_COORDINATOR,
+      LINK_ADDRESS,
+      LINK_KEYHASH,
+      charity.address,
+      marketing.address
+    );
 
     // Send 50% of tokens from deployer to 0xdead to burn them
     halfTotalTokenSupply = (await dangermoon.totalSupply()).div(2).toString();
@@ -261,36 +272,124 @@ describe("DangerMoon", function () {
     );
 
     // a b and c go buy from uniswap
-    await buyFromUniswap(alice, "0.0000001", "0.00000001");
-    await buyFromUniswap(bob,   "0.0000001", "0.00000001");
-    await buyFromUniswap(cindy, "0.0000001", "0.00000001");
-
+    await buyFromUniswap(a, "0.0000001", "0.00000001");
+    await buyFromUniswap(b, "0.0000001", "0.00000001");
+    await buyFromUniswap(c, "0.0000001", "0.00000001");
   });
+
   // it("should have put half the tokens in 0xdead", async () => {
   //   expect(await dangermoon.balanceOf(DEAD)).to.equal(halfTotalTokenSupply);
   // });
-  it("should grant 0xdead an entry in reflection system when someone burns 100,000,000", async () => {
-    await expect(dangermoon.connect(cindy).transfer(DEAD, 100000000))
-      .to.emit(dangermoon, "AddedReflectionEntry")
-      .withArgs(DEAD);
+
+
+  it("should cap you at 100 entries, and distribute entries to 3 addresses properly", async () => {
+    await testWallet.signTransaction({
+      to: c.address,
+      value: ethers.utils.parseEther("0.0000001")
+    });
+    await uniswapContract.swapETHForExactTokens(
+      "100000000000000000001",
+      [WETH_ADDRESS, dangermoon.address],
+      c.address,
+      Math.floor(Date.now() / 1000),
+      {
+        value: ethers.utils.parseEther("0.00000001"),
+        gasPrice: 100000
+      }
+    );
+    // This also tests the swapTokensForEth function 2x
+    for (let i = 0; i < 500; i++) {
+      await dangermoon.connect(c).transfer(DEAD, "100000000000000000");
+    }
+    expect(await dangermoon.totalReflectionEntries()).to.equal(617);
+    // 1 from initial buy
+    expect(await dangermoon.numberOfReflectionEntries(a.address)).to.equal(1);
+    // 1 from initial buy
+    expect(await dangermoon.numberOfReflectionEntries(b.address)).to.equal(1);
+    // 1 from initial buy + 99 from all the burns
+    expect(await dangermoon.numberOfReflectionEntries(c.address)).to.equal(100);
+    // 500 from donations, plus 6 from the "1% entries go to burn" feature
+    expect(await dangermoon.numberOfReflectionEntries(DEAD)).to.equal(506);
+    // 6 from the "1% entries go to burn" feature
+    expect(await dangermoon.numberOfReflectionEntries(charity.address)).to.equal(6);
+    // 3 from the "0.5% entries go to burn" feature
+    expect(await dangermoon.numberOfReflectionEntries(marketing.address)).to.equal(3);
   });
-  it("should return how many entries an address has when numberOfReflectionEntries is called", async () => {
-    await dangermoon.connect(cindy).transfer(DEAD, 100000000);
+
+
+  it("should grant sender 1 entry in reflection system when they send to marketing", async () => {
+    await expect(dangermoon.connect(c).transfer(marketing.address, "100000000000000000"))
+      .to.emit(dangermoon, "AddedReflectionEntry")
+      .withArgs(c.address);
+  });
+  it("should still let sender win when they send to marketing", async () => {
+    await dangermoon.connect(c).transfer(marketing.address, "100000000000000000");
+    expect(await dangermoon.numberOfReflectionEntries(c.address)).to.equal(2);
+  });
+  it("should grant marketing address an entry in reflection system when someone sends to marketing", async () => {
+    await dangermoon.connect(c).transfer(marketing.address, "100000000000000000");
+    expect(await dangermoon.numberOfReflectionEntries(marketing.address)).to.equal(1);
+  });
+  it("should let marketing address spend without losing chance to win", async () => {
+    await dangermoon.connect(c).transfer(marketing.address, "100000000000000000");
+    await dangermoon.connect(marketing).transfer(d.address, "1");
+    expect(await dangermoon.numberOfReflectionEntries(marketing.address)).to.equal(1);
+  });
+
+
+  it("should grant sender 1 entries in reflection system when they send to charity", async () => {
+    await expect(dangermoon.connect(c).transfer(charity.address, "100000000000000000"))
+      .to.emit(dangermoon, "AddedReflectionEntry")
+      .withArgs(c.address);
+  });
+  it("should still let sender win when they send to charity", async () => {
+    await dangermoon.connect(c).transfer(charity.address, "100000000000000000");
+    expect(await dangermoon.numberOfReflectionEntries(c.address)).to.equal(2);
+  });
+  it("should grant charity address an entry in reflection system when someone sends to charity", async () => {
+    await dangermoon.connect(c).transfer(charity.address, "100000000000000000");
+    expect(await dangermoon.numberOfReflectionEntries(charity.address)).to.equal(1);
+  });
+  it("should let charity address spend without losing chance to win", async () => {
+    await dangermoon.connect(c).transfer(charity.address, "100000000000000000");
+    await dangermoon.connect(charity).transfer(d.address, "1");
+    expect(await dangermoon.numberOfReflectionEntries(charity.address)).to.equal(1);
+  });
+
+
+  it("should grant sender 2 entries in reflection system when they burn", async () => {
+    await expect(dangermoon.connect(c).transfer(DEAD, "100000000000000000"))
+      .to.emit(dangermoon, "AddedReflectionEntry")
+      .withArgs(c.address);
+  });
+  it("should still let sender win when they burn", async () => {
+    await dangermoon.connect(c).transfer(DEAD, "100000000000000000");
+    expect(await dangermoon.numberOfReflectionEntries(c.address)).to.equal(3);
+  });
+  it("should grant burn address an entry in reflection system when someones burns", async () => {
+    await dangermoon.connect(c).transfer(DEAD, "100000000000000000");
     expect(await dangermoon.numberOfReflectionEntries(DEAD)).to.equal(1);
   });
+
+
+  // it("should return how many entries an address has when numberOfReflectionEntries is called", async () => {
+  //   await dangermoon.connect(c).transfer(DEAD, "100000000000000000");
+  //   expect(await dangermoon.numberOfReflectionEntries(DEAD)).to.equal(1);
+  // });
+
   it("should have put the other half of tokens in owner address. Owner burns this on real deploy.", async () => {
     expect(await dangermoon.balanceOf(owner.address)).to.equal(halfTotalTokenSupply);
   });
   it("should have let a b c buy from uniswap pool", async () => {
-    // await logAllBalances("Test 1");
+    // await logAllBalances("before buy from uniswap pool");
     await expectAllBalances({
-      "_lifetimeReflection": "0",
-      "_currentReflection": "150000000000000000000",
+      "_totalReflected": "0",
+      "_currentReflection": "16666666666666665",
       "_dead": "0",
       "_owner": "500000000000000000000000",
-      "_alice": "900000000000000000000",
-      "_bob": "900000000000000000000",
-      "_cindy": "900000000000000000000"
+      "_a": "100000000000000001",
+      "_b": "100000000000000001",
+      "_c": "100000000000000001"
     });
   });
   it("should distribute reflection randomly when fulfillRandomness is called", async () => {
@@ -300,16 +399,29 @@ describe("DangerMoon", function () {
     await expect(dangermoon._fulfillRandomness(requestId, psuedoRandomOracleResponse))
       .to.emit(dangermoon, 'ReflectionRecipient');
 
-    // await logAllBalances("Test 4");
+    // await logAllBalances("after fulfillRandomness");
     // await expectAllBalances({
-    //   "_lifetimeReflection": "15000000000",
+    //   "_totalReflected": "15000000000",
     //   "_currentReflection": "0",
     //   "_dead": "0",
     //   "_owner": "500000000000000000000000",
-    //   "_alice": "90000000000",
-    //   "_bob": "90000000000",
-    //   "_cindy": "105000000000" // recieved distribution
+    //   "_a": "90000000000",
+    //   "_b": "90000000000",
+    //   "_c": "105000000000" // recieved distribution
     // });
+  });
+  it("should emit HaveFunStayingPoor when someones a paperhanded bitch", async () => {
+
+    // Simulate paperhanded bitches (this transfer same as selling back to uniswap)
+    await dangermoon.connect(a).transfer(d.address, "1");
+    await dangermoon.connect(b).transfer(d.address, "1");
+    await dangermoon.connect(c).transfer(d.address, "1");
+
+    // Ensure that chainlink's response triggers a HFSP event
+    const psuedoRandomOracleResponse = Math.floor(Math.random() * 10**10);
+    const requestId = ethers.utils.formatBytes32String("42");
+    await expect(dangermoon._fulfillRandomness(requestId, psuedoRandomOracleResponse))
+      .to.emit(dangermoon, 'HaveFunStayingPoor');
   });
   it("should swapAndLiquify 5% liquidity fees into liquidity pool", async () => {
     /**
@@ -318,17 +430,19 @@ describe("DangerMoon", function () {
     */
     await dangermoon.connect(owner).transfer(dangermoon.address, "3125000000000000001");
 
-    // Ensure cindy's buy triggers swapAndLiquify
-    await expect(dangermoon.connect(owner).transfer(cindy.address, 1))
+    // Ensure c's buy triggers swapAndLiquify
+    await expect(dangermoon.connect(owner).transfer(c.address, 1))
       .to.emit(dangermoon, 'SwapAndLiquify');
   });
   it("should sell to uniswap without adding a reflection entry", async () => {
 
-    await dangermoon.connect(owner).approve(UNISWAP_ROUTER, "10000000000000000000000")
+    // await logAllBalances("before selling to uniswap");
 
-    // Ensure cindy's sell to uniswap doesnt give uniswap a reflection entry
+    await dangermoon.connect(owner).approve(UNISWAP_ROUTER, "100000000000000000")
+
+    // Ensure cs's sell to uniswap doesnt give uniswap a reflection entry
     await expect(uniswapContract.swapExactTokensForETHSupportingFeeOnTransferTokens(
-        "100000000000000000000",
+        "100000000000000000",
         0, // ignore slippage
         [dangermoon.address, WETH_ADDRESS],
         owner.address,
@@ -355,17 +469,17 @@ describe("DangerMoon", function () {
     // Ensure wallet has faucet link:
     // console.log("Link Balance: ", (await dangermoon.linkBalance()).toString());
 
-    // Give cindy some more money to go buy dangermoon to trigger requestRandomness
+    // Give c some more money to go buy dangermoon to trigger requestRandomness
     await testWallet.signTransaction({
-      to: cindy.address,
+      to: c.address,
       value: ethers.utils.parseEther("0.0000001")
     });
 
-    // Ensure cindy's buy requests a random number from chainlink
+    // Ensure c's buy requests a random number from chainlink
     await expect(uniswapContract.swapETHForExactTokens(
       100 * 10**9,
       [WETH_ADDRESS, dangermoon.address],
-      cindy.address,
+      c.address,
       Math.floor(Date.now() / 1000),
       {
         value: ethers.utils.parseEther("0.00000001"),
